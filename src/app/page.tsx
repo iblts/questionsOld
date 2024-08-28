556fc4687b9b6e9694891e8a353e6bc04b8892ae
Ivan Belyaev <ibelyaevnmc@gmail.com>
2024-08-28 15:38:12 +0700

    fixed fetch urls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
 	let modules: ModuleWithRelations[] = []
 
 	try {
-		const response = await fetch(`${process.env.API_URL}/module`, {
+		const response = await fetch(`api/module`, {
 			cache: 'no-cache',
 		})
 		modules = await response.json()
