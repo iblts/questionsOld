6d0bbd96abfc4225fa1a6bce5f5bf143f9628c9e
iblts <159683822+iblts@users.noreply.github.com>
2024-08-28 12:41:27 +0700

    Update route.ts

--- src/app/api/card/route.ts
+++ src/app/api/card/route.ts
@@ -1,9 +1,8 @@
 import { Card } from '@prisma/client'
-import { NextApiRequest } from 'next'
 import { NextRequest } from 'next/server'
 import prisma from '../../../../lib/prisma'
 
-export async function GET(request: NextApiRequest) {
+export async function GET(request: NextRequest) {
 	const card = await prisma.card.findMany()
 
 	return Response.json({ card })
